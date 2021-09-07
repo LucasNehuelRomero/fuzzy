49c6d7c6469db109b571a49153882ba824783e87
unknown <romerolucas561@gmail.com>
2021-09-07 10:17:37 -0300

    commit login

--- src/componentes/Login.js
+++ src/componentes/Login.js
@@ -1,10 +1,27 @@
+//------------Parámetros-----------------------------------------------
+const endpoint = "https://accounts.spotify.com/authorize"
+
+const loginURL = `${endpoint}?client_id${clientID}&response_type-token&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&show-dialog=true`
+
+const clientID = "f24ab3a85bb648afa08f27061f22b28e"
+
+const redirectUri = "http://localhost:3000"
+
+const scopes = ["user-read-currently-playing",
+"user-read-recently-played",
+"user-read-playback-state",
+"user-top-read",
+"user-modify-playback-state",]
+//--------------...-------------------------------------------------------
 
 const Login = () => {
 return (
-  <div classname="login">
-    <h1>Fuzzy</h1>
-    <button className="botonLogin">Iniciar sesión</button>
+  <div className="loginContainer">
+    <h1>Login</h1>
+    <button className="loginButton" href={loginURL}>login with fuzzy</button>
   </div>
-);
+  )
 }
+
 export default Login;
+
